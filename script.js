5f54f14b7b2490a19d2c2f49af04d2d55370b67c
Ramiro <GARZA.RAMIROV@GMAIL.COM>
2023-06-28 18:10:33 -0600

    Add function to update gameboard each time a space is clicked, working towards win conditions

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const Player = (sign) => {
     return { getSign }
 };
 
-const gameBoard = (() =>{
+const gameBoard = (() => {
     const board = ["", "", "", "", "", "", "", "", ""];
 
     const setField = (index, sign) => {
@@ -15,12 +15,38 @@ const gameBoard = (() =>{
         return board[index];
     }
 
-    const resetBoard = () => {
+    const reset = () => {
         for (let i = 0; i < board.length; i++) {
             board[i] = "";
         }
     }
 
-    return { setField, getField, resetBoard };
+    return { setField, getField, reset };
+})();
+
+const displayGame = (() => {
+    const spaces = document.querySelectorAll(".space");
+    const reset = document.getElementById("reset");
+
+    spaces.forEach((space) =>
+        space.addEventListener("click", (e) => {
+            if (gameController.getIsOver() || e.target.textContent !== "") return;
+            gameController.playRound(parseInt(e.target.dataset.index));
+            updateGameboard();
+        })
+    );
+
+    reset.addEventListener("click", (e) => {
+        gameBoard.reset();
+        gameController.reset();
+        updateGameboard();
+    })
+
+    const updateGameboard = () => {
+        for (let i = 0; i < spaces.length; i++) {
+            spaces[i].textContent = gameBoard.getField(i);
+        }
+    }
+
 })();
 
