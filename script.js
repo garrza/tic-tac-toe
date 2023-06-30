92d80be8bbc2605d6b3f9249dbf261cf04816e0d
Ramiro <GARZA.RAMIROV@GMAIL.COM>
2023-06-30 16:33:47 -0600

    Logic mostly there i guess

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const Player = (sign) => {
     const getSign = () => sign;
 
-    return { getSign }
+    return { getSign };
 };
 
 const gameBoard = (() => {
@@ -9,44 +9,117 @@ const gameBoard = (() => {
 
     const setField = (index, sign) => {
         board[index] = sign;
-    }
+    };
 
     const getField = (index) => {
         return board[index];
-    }
+    };
 
     const reset = () => {
         for (let i = 0; i < board.length; i++) {
             board[i] = "";
         }
-    }
+    };
 
     return { setField, getField, reset };
 })();
 
 const displayGame = (() => {
     const spaces = document.querySelectorAll(".space");
-    const reset = document.getElementById("reset");
+    const resetButton = document.getElementById("reset");
+    const info = document.getElementById("info");
 
     spaces.forEach((space) =>
         space.addEventListener("click", (e) => {
-            if (gameController.getIsOver() || e.target.textContent !== "") return;
-            gameController.playRound(parseInt(e.target.dataset.index));
+            if (gameFlow.getIsOver() || e.target.textContent !== "") return;
+            gameFlow.playRound(parseInt(e.target.dataset.index));
             updateGameboard();
         })
     );
 
-    reset.addEventListener("click", (e) => {
+    resetButton.addEventListener("click", (e) => {
         gameBoard.reset();
-        gameController.reset();
+        gameFlow.reset();
         updateGameboard();
-    })
+        setMessageElement("Player X's turn")
+    });
 
     const updateGameboard = () => {
         for (let i = 0; i < spaces.length; i++) {
             spaces[i].textContent = gameBoard.getField(i);
         }
-    }
+    };
 
+    const setResultMessage = (winner) => {
+        if (winner === "Draw") {
+            setMessageElement("It's a draw!");
+        } else {
+            setMessageElement(`Player ${winner} wins!`);
+        }
+    };
+
+    const setMessageElement = (message) => {
+        info.textContent = message;
+    };
+
+    return { setResultMessage, setMessageElement };
 })();
 
+const gameFlow = (() => {
+    const playerX = Player("X");
+    const playerO = Player("O");
+    let round = 1;
+    let isOver = false;
+
+    const playRound = (fieldIndex) => {
+        gameBoard.setField(fieldIndex, currentPlayerSign());
+        if (checkWinner(fieldIndex)) {
+            displayGame.setResultMessage(currentPlayerSign());
+            isOver = true;
+            return;
+        }
+        if (round === 9) {
+            displayGame.setResultMessage("Draw");
+            isOver = true;
+            return;
+        }
+        round++;
+        displayGame.setMessageElement(`Player ${getCurrentPlayerSign()}'s turn`);
+    };
+
+    const currentPlayerSign = () => {
+        return round % 2 === 1 ? playerX.getSign() : playerO.getSign();
+    };
+
+    const checkWinner = (fieldIndex) => {
+        const winConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        return winConditions
+            .filter((combination) => combination.includes(fieldIndex))
+            .some((possibleCombination) =>
+                possibleCombination.every(
+                    (index) => gameBoard.getField(index) === currentPlayerSign()
+                )
+            );
+    };
+
+    const getIsOver = () => {
+        return isOver;
+    };
+
+    const reset = () => {
+        round = 1;
+        isOver = false;
+    };
+
+    return { playRound, getIsOver, reset };
+})();
